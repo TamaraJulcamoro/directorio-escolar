90f75ad36935290dc57383b7ba83f585ea6c96f6
TamaraJulcamoro <alejajulcamoro@gmail.com>
2026-08-11 12:33:38 -0500

    Corrige carga y actualización del directdfdorio93

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+// Coloca los valores entre comillas sencillas o dobles
+const SUPABASE_URL = 'https://mafeychncqarhasjfjsk.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hZmV5Y2huY3Fhcmhhc2pmanNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY0NTM2NzksImV4cCI6MjEwMjAyOTY3OX0.FOCWBGt5HmcK32KmwnvIfbB2l7qk1Y0xZOUdwlAEwyE';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
